579490510134d45b54a30a7ce9519f642ad15247
galen <galen.ballew@gmail.com>
2021-01-04 15:49:33 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1e58557e76d481576d80.js"
+    "url": "webpack-runtime-94de522f59e723d4696b.js"
   },
   {
     "url": "framework-cd3e1e804d552fa282ef.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "eddca216fc988dbeae339b30235e36db"
+    "revision": "f4bdc90e7a453ee707f362a38d42d638"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
